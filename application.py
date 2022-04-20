f16ee67fa8d6db973d66bb01998fba67546cac97
Mohamed shafei <abdm90486@gmail.com>
2022-04-20 22:21:38 +0300

    add chaotic algorithm

--- application.py
+++ application.py
@@ -3,7 +3,14 @@ import os
 from skimage import color
 from skimage import io
 from PIL import Image
-
+from Crypto.Cipher import DES3
+from hashlib import md5
+from helper import Encryption
+from helper import Decryption
+import cv2
+import math
+import numpy as np
+from flask import send_from_directory
 
 
 app = Flask(__name__)
@@ -19,8 +26,10 @@ def allowed_file(filename):
     return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
 
 @app.route('/')
+
 def index():
     return render_template("index.htm")
+
 
 @app.route('/encryption',methods=['POST'])
 def encryption():
@@ -38,7 +47,14 @@ def encryption():
         img = Image.open(imgPath)
         imgGray = img.convert('L')
         imgGray.save(imgPath)
-        return render_template("index.htm",filename=filename)
+        img = cv2.imread(imgPath, 1)   
+        
+        EncryptionImg = np.zeros(img.shape, np.uint8)
+        Encryption(img,10,30,0.123345,EncryptionImg)                                       # encryption
+        cv2.imwrite(f"./static/uploads/encrypted{filename}",EncryptionImg)  # Save the encrypted image
+        #send_from_directory("./static/uploads/", filename=f'encrypted{filename}')
+        print('Operation Done!')
+        return render_template("index.htm",filename=f'encrypted{filename}')
     else:
         flash("Allowed image types are -> png, jpg, jpeg, gif")
         return render_template("index.htm")
@@ -47,6 +63,7 @@ def encryption():
 
 @app.route('/decryption',methods=['POST'])
 def decryption():    
+
     if 'file' not in request.files:
         flash("No Images Found")
         return redirect(request.url)
@@ -58,14 +75,22 @@ def decryption():
         filename = file.filename
         imgPath = os.path.join(app.config['UPLOAD_FOLDER'], filename) # get the path of the image
         file.save(imgPath)
-        print(filo)
-        img = Image.open(imgPath)
-        imgGray = img.convert('L')
-        imgGray.save(imgPath)
-        return render_template("index.htm",filename=filename)
+        # img = Image.open(imgPath)
+        # imgGray = img.convert('L')
+        # imgGray.save(imgPath)
+
+        img = cv2.imread(imgPath, 1)        # Read decrypted image
+        print(img)
+        DecryptionImg = np.zeros(img.shape, np.uint8)
+        Decryption(img, 10, 30, 0.123345, DecryptionImg)                                   # decrypt
+        cv2.imwrite(f"./static/uploads/decrypted{filename}", DecryptionImg) # Save the decrypted image
+        
+        return render_template("index.htm",filename=f'decrypted{filename}')
+       
     else:
         flash("Allowed image types are -> png, jpg, jpeg, gif")
         return render_template("index.htm")
+
 
 
 if __name__ == "__main__":
--- helper.py
+++ helper.py
@@ -1,5 +1,68 @@
-def encryption(image):
-  pass;
+import cv2
+import math
+import numpy as np
 
-def decryption(image):
-  pass
+
+def int2bin8(x):                               # Integer to 8-bit binary
+    result=""
+    for i in range(8):
+        y=x&(1)
+        result+=str(y)
+        x=x>>1
+    return result[::-1]
+
+def int2bin16(x):                              # Integer to 8-bit binary
+    result=""
+    for i in range(16):
+        y=x&(1)
+        result+=str(y)
+        x=x>>1
+    return result
+def Encryption(img,j0,g0,x0,EncryptionImg):
+    x = img.shape[0]
+    y = img.shape[1]
+    c = img.shape[2]
+    g0 = int2bin16(g0)
+    for s in range(x):
+        for n in range(y):
+            for z in range(c):
+                m = int2bin8(img[s][n][z])                   # Pixel value to octet binary
+                ans=""
+                # print("ok")
+                for i in range(8):
+                    ri=int(g0[-1])                           # Take the last digit of the manual cipher machine
+                    qi=int(m[i])^ri                          # XOR with pixel value qi
+                    xi = 1 - math.sqrt(abs(2 * x0 - 1))      # f1(x) chaotic iteration
+                    if qi==0:                                # If qi=0, use x0i+x1i=1;
+                        xi=1-xi
+                    x0=xi                                    # xi iteration
+                    t=int(g0[0])^int(g0[12])^int(g0[15])     # Primitive polynomial x^15+x^3+1
+                    g0=str(t)+g0[0:-1]                       # gi iteration
+                    ci=math.floor(xi*(2**j0))%2              # Nonlinear transformation operator
+                    ans+=str(ci)
+                re=int(ans,2)
+                EncryptionImg[s][n][z]=re                    # Write new image
+def Decryption(img, j0, g0, x0, DecryptionImg):
+    x = img.shape[0]
+    y = img.shape[1]
+    c = img.shape[2]
+    g0 = int2bin16(g0)
+    for s in range(x):
+        for n in range(y):
+            for z in range(c):
+                cc = int2bin8(img[s][n][z])
+                ans = ""
+                # print("no")
+                for i in range(8):
+                    xi = 1 - math.sqrt(abs(2 * x0 - 1))
+                    x0 = xi
+                    ssi = math.floor(xi * (2 ** j0)) % 2
+                    qi=1-(ssi^int(cc[i]))
+                    ri = int(g0[-1])
+                    mi=ri^qi
+                    t = int(g0[0]) ^ int(g0[12]) ^ int(g0[15])
+                    g0 = str(t) + g0[0:-1]
+                    ans += str(mi)
+                re = int(ans, 2)
+                DecryptionImg[s][n][z] = re
+
