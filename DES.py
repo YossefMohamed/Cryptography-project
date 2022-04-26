87ab46e0a79415102befa4d5fb3b364adb0897d0
Mohamed-Abd-El-Nasser <47921242+Mohamed-Abd-El-Nasser@users.noreply.github.com>
2022-04-26 12:34:19 +0300

    convert images to gray scale

--- DES.py
+++ DES.py
@@ -1,5 +1,7 @@
 from Crypto.Cipher import DES3
 from hashlib import md5
+from PIL import Image
+
 
 while True:
     print('Which operation do you want ? :\n\t1- Encryption\n\t2- Decryption')
@@ -16,7 +18,10 @@ while True:
     tdes_key = DES3.adjust_key_parity(key_hash)
     
     cipher = DES3.new(tdes_key, DES3.MODE_CTR, nonce=b'0')
-
+    if(wanted_operation == '1'):
+        img = Image.open(image_path)
+        imgGray = img.convert('L')
+        imgGray.save(image_path)
     with open(image_path, 'rb') as input_image:
         image_bytes = input_image.read()
         
